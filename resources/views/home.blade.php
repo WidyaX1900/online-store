4ec7a21fdcae1ef1c071312554a796607da80c68
WidyaX1900 <tuderangga93@gmail.com>
2023-01-02 09:03:33 +0800

    Update home.blade.php links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home Page</title>
-    <link rel="stylesheet" href="http://localhost/online-store/resources/css/bootstrap.css" />
-    <link rel="stylesheet" href="http://localhost/online-store/resources/css/home.css" />
+    <link rel="stylesheet" href="{{ config('app.url').'/online-store/resources/css/bootstrap.css' }}" />
+    <link rel="stylesheet" href="{{ config('app.url').'/online-store/resources/css/home.css' }}" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="icon" href="img/logo.png">
   </head>
@@ -180,5 +180,5 @@
     integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
     crossorigin="anonymous"
   ></script>
-  <script src="js/home.js"></script>
+  <script src="{{ config('app.url').'/online-store/resources/js/home.js' }}"></script>
 </html>
