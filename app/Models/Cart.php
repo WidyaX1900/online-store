4bc2ff84797c466260df0fa8e3974f3b5fd6079b
WidyaX1900 <tuderangga93@gmail.com>
2023-01-06 10:12:53 +0800

    Create new Models/Cart.php and cart tables migration files. Update info.blade.php

---
+++ app/Models/Cart.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'buyer_id',
+        'purchase_date',
+        'purchase_amount'
+    ];
+}
--- resources/views/product/info.blade.php
+++ resources/views/product/info.blade.php
@@ -31,12 +31,48 @@
                     </select>
                 </div>
                 <button type="button">Buy Now</button>
-                <button type="button">
+                <button type="button" data-bs-toggle="modal" data-bs-target="#cartModal">
                     <span class="material-symbols-outlined">
                         shopping_cart
                     </span>
                     Add to cart
                 </button>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="cartModalLabel">Add to Cart</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div id="cart-form">
+                    <form action="" method="post">
+                        @csrf
+                        <input type="hidden" name="product_id" id="product_id" value="{{ $product->id }}">
+                        <input type="hidden" name="buyer_id" id="buyer_id" value="{{ Auth::user()->id }}">
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Product Name</label>
+                                <input type="text" id="name" class="form-control" readonly value="{{ $product->name }}">   
+                            </div>
+                            <div class="mb-3">
+                                <label for="cost" class="form-label">Cost</label>
+                                <input type="text" id="cost" class="form-control" readonly name="cost" value="{{ $product->cost }}">   
+                            </div>
+                            <div class="mb-3">
+                                <label for="purchase_amount" class="form-label">Purchase Amount</label>
+                                <input type="text" id="purchase_amount" class="form-control" readonly name="purchase_amount">   
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outlined-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Add to cart</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
             </div>
         </div>
     </section>
